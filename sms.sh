fef312db9933cc22a4065ddbcfa9b827e3fc2de5
Rickkwa <rickyk.162@gmail.com>
2016-10-15 18:24:55 -0400

    Added comments

--- sms.sh
+++ sms.sh
@@ -2,9 +2,9 @@
 readonly ACCOUNT_SID=''
 readonly AUTH_TOKEN=''
 
-to="$1"
-from="$2"
-text="$3"
+to="$1" # Should contain the value of {ALERT.SENDTO}
+from="$2" # Should contain your Twilio phone number
+text="$3" # Should container the value of {ALERT.MESSAGE} or {ALERT.SUBJECT}
 
 curl -X POST "https://api.twilio.com/2010-04-01/Accounts/${ACCOUNT_SID}/Messages.json" \
     --data-urlencode "To=${to}" \
